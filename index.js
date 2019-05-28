3179666552165f5728790a0a78a40e0da1cd60fe
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-28 19:38:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  
+  static sanitize(str) {
+    let st = ''
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "-" || str[i] === "'" || str[i] === " ") {
+        str += str[i]
+      }
+    }
+    return st
+  }
+
 }
+
+Formatter.capitalize()
+// Formatter.sanitize()
