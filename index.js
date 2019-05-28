fd19c8ae23d9c619aa2a917dd4ba12c2efa8e828
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-28 19:42:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,24 @@ class Formatter {
    static sanitize( string ) {
     return string.replace( /[^A-Za-z0-9 '-]/g, '' )
   }
+  
+  static titleize( sentence ) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = [];
+    let arrayOfWords = sentence.split(" ")
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if (n === 0) {
+        result.push(this.capitalize(arrayOfWords[n]))
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
+
+    }
+    return result.join( " " );
+  }
 
 }
